c4365289eb04bede830a71b6b35330b510c7d3a1
TotallyNotRust <35370673+TotallyNotRust@users.noreply.github.com>
2021-03-01 12:42:23 +0100

    Revert "hej"
    This reverts commit 702ed32156b10cf889fd57d6b566a10b96a877b5.

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import string
+
+class Calculator:
+    def calculate(self):
+        while True:
+            calculate = input("Gib math: ")
+            if all([i in "1234567890+-/*()" for i in calculate]):
+                print(eval(calculate))
+            else:
+                print("Not math?!")
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    while True:
+        calculator.calculate()
