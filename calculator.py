fae139b4a60fc6a395826d1cfa1cfc4336258385
TotallyNotRust <35370673+TotallyNotRust@users.noreply.github.com>
2021-03-10 11:03:33 +0100

    Changed your comment.
    Changed the comment to be more discriptive.

--- calculator.py
+++ calculator.py
@@ -9,7 +9,8 @@ class Calculator:
             else:
                 print("Not math?!")
 
-# __name__ predefined variable which is equal to __main__ if this is ran directly in the program.
+# __name__ is equal to "__main__" if the app is run directly
+# If instead the code is ran through an import in another program it is going to be equal to the name of the file
 if __name__ == "__main__":
     calculator = Calculator()
     while True:
